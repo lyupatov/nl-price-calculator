f9026e6e4ac7870e500d7f4a29ea24064db628f3
lyupatov <youngbuckwheat@gmail.com>
2026-01-15 17:40:41 +0300

    add logging

--- src/api/api.py
+++ src/api/api.py
@@ -1,18 +1,28 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
 from src.schemas.schemas import MaterialsData
 from src.db.session import get_session
 from src.services.services import commit_calc_materials
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, desc
 from src.db.models import CalcResults
+from src.core.logger import logger
 
 router = APIRouter()
 
 
 @router.post("/calc")
 async def calc(data: MaterialsData, session: AsyncSession = Depends(get_session)):
-    total_cost_rub = await commit_calc_materials(data, session)
-    return {"total_cost_rub": total_cost_rub}
+    try:
+        total_cost_rub = await commit_calc_materials(data, session)
+        return {"total_cost_rub": total_cost_rub}
+
+    except ValueError as e:
+        logger.warning("Validation error: %s", e)
+        raise HTTPException(status_code=400, detail=str(e))
+
+    except Exception as e:
+        logger.warning("Internal server error")
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @router.get("/costs_list")
--- src/services/services.py
+++ src/services/services.py
@@ -1,12 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.core.logger import logger
 from src.db.models import CalcResults
 
 
 async def commit_calc_materials(data, session: AsyncSession):
-    total_cost_rub = sum(mat.qty * mat.price_rub for mat in data.materials)
-    calc = CalcResults(total_cost_rub=total_cost_rub)
-    session.add(calc)
-    await session.commit()
-    await session.refresh(calc)
+    try:
+        total_cost_rub = sum(mat.qty * mat.price_rub for mat in data.materials)
+        calc = CalcResults(total_cost_rub=total_cost_rub)
+        session.add(calc)
+        await session.commit()
+        await session.refresh(calc)
 
-    return total_cost_rub
+        return total_cost_rub
+    except Exception as e:
+        await session.rollback()
+        logger.exception("Failed to save calc result")
+        raise
